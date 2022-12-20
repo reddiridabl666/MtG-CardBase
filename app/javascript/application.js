5a8776632d2076ed318b6367bc868179db814ebc
reddiridabl666 <reddiridabl666@gmail.com>
2022-12-20 23:12:11 +0300

    Added form frontend

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,2 +1,10 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import 'bootstrap'
+// import "@hotwired/turbo-rails"
+
+import { Turbo } from "@hotwired/turbo-rails"
+Turbo.session.drive = false
+
+import "controllers"
+
+import "bootstrap"
+import "popper";
