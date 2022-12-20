67771566fb5b18a0bf3463aed4244331522f83ba
reddiridabl666 <reddiridabl666@gmail.com>
2022-12-20 19:28:56 +0300

    Added more styles and collapse/expand buttons

--- Gemfile
+++ Gemfile
@@ -51,6 +51,8 @@ gem 'image_processing', '~> 1.2'
 
 gem 'kaminari'
 
+gem 'popper_js'
+
 # Use Active Storage variants [https://guides.rubyonrails.org/active_storage_overview.html#transforming-images]
 # gem "image_processing", "~> 1.2"
 
--- app/javascript/show-extended.js
+++ app/javascript/show-extended.js
@@ -1,17 +1,43 @@
-for (let btn of document.getElementsByClassName('dropdown-btn')) {
-    let card_id = btn.id.substring(btn.id.indexOf('-'));
+function dropdown_buttons() {
+    return document.getElementsByClassName('dropdown-btn');
+}
 
-    // let base_id = 'base' + card_id;
-    let extended_id = 'extended' + card_id;
+for (let btn of dropdown_buttons()) {
+    let card = cardByBtn(btn);
 
     btn.addEventListener('click', () => {
-        // let base = document.getElementById(base_id)
-        let extended = document.getElementById(extended_id)
-
-        if (extended.style.display === 'none') {
-            extended.style.display = 'block';
+        if (!card.classList.contains('active')) {
+            showCard(card, btn);
         } else {
-            extended.style.display = 'none';
+            hideCard(card, btn);
         }
     });
 }
+
+function showCard(card, btn) {
+    btn.classList.add('rotated');
+    card.classList.add('active');
+}
+function hideCard(card, btn) {
+    btn.classList.remove('rotated');
+    card.classList.remove('active');
+}
+
+function cardByBtn(btn) {
+    let card_id = btn.id.substring(btn.id.indexOf('-'));
+    let extended_id = 'extended' + card_id;
+    return document.getElementById(extended_id)
+}
+
+document.getElementById('collapse-btn').addEventListener('click', () => {
+    for (let btn of dropdown_buttons())  {
+        hideCard(cardByBtn(btn), btn);
+    }
+});
+
+document.getElementById('expand-btn').addEventListener('click', () => {
+    for (let btn of dropdown_buttons())  {
+        showCard(cardByBtn(btn), btn);
+    }
+});
+
